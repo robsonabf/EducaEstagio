8e129a23c57d6cc4818fd7d7bae64e0a9fda1f61
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 17:16:23 -0300

    Update settings.py

--- educaestagio/educaestagio/settings.py
+++ educaestagio/educaestagio/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'educa',
+    'educaestagio.educa',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

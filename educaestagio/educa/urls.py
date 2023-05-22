d800bce5316c57765fe3c0eb8308d760beae3b74
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 17:52:17 -0300

    Update urls.py

--- educaestagio/educa/urls.py
+++ educaestagio/educa/urls.py
@@ -1,1 +1,23 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from .views import home, sobre, registro,\
+    UsuarioCreate, CurriculoCreate, CurriculoUpdate, CurriculoDelete, CurriculoList
 
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('sobre/', sobre, name='sobre'),
+    path('registro/', registro, name='registro'),
+
+    path('accounts/', include('allauth.urls')),
+
+    path('signup/', UsuarioCreate.as_view(template_name='account/signup.html'), name='registrar'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('formCurriculo/createCurriculo/', CurriculoCreate.as_view(), name='createCurriculo'),
+    path('candidato/', CurriculoList.as_view(), name='candidato'),
+    path('formCurriculo/editCurriculo/<int:pk>/', CurriculoUpdate.as_view(), name='editCurriculo'),
+    path('formCurriculo/delCurriculo/<int:pk>/', CurriculoDelete.as_view(), name='delCurriculo'),
+
+]

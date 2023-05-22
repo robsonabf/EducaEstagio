bc2be232fca2e26290e7afa6b01fe2aea137763b
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 18:43:07 -0300

    Update urls.py

--- educaestagio/educa/urls.py
+++ educaestagio/educa/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('formCurriculo/editCurriculo/<int:pk>/', CurriculoUpdate.as_view(), name='editCurriculo'),
     path('formCurriculo/delCurriculo/<int:pk>/', CurriculoDelete.as_view(), name='delCurriculo'),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

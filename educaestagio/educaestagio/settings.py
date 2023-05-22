ea61fb5067382a3af40a42c1cd2338846a21e54c
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 18:04:02 -0300

    Update settings.py

--- educaestagio/educaestagio/settings.py
+++ educaestagio/educaestagio/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'educaestagio.urls'
+ROOT_URLCONF = 'educaestagio.educaestagio.urls'
 
 TEMPLATES = [
     {

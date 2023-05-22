78fc4c8d653da9b40400b024e298bf9c7349c926
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 17:52:41 -0300

    Update urls.py

--- educaestagio/educaestagio/urls.py
+++ educaestagio/educaestagio/urls.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from educaestagio.educa.views import home, sobre, registro,\
-    UsuarioCreate, CurriculoCreate, CurriculoUpdate, CurriculoDelete, CurriculoList
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('sobre/', sobre, name='sobre'),
-    path('registro/', registro, name='registro'),
-
-    path('accounts/', include('allauth.urls')),
-
-    path('signup/', UsuarioCreate.as_view(template_name='account/signup.html'), name='registrar'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path('formCurriculo/createCurriculo/', CurriculoCreate.as_view(), name='createCurriculo'),
-    path('candidato/', CurriculoList.as_view(), name='candidato'),
-    path('formCurriculo/editCurriculo/<int:pk>/', CurriculoUpdate.as_view(), name='editCurriculo'),
-    path('formCurriculo/delCurriculo/<int:pk>/', CurriculoDelete.as_view(), name='delCurriculo'),
-
+    path('', include('educa.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

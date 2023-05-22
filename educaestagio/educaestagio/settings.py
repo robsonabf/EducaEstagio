4c5bbec45e28994b6f63b982deee72a56defff47
robsonabf <32685128+robsonabf@users.noreply.github.com>
2023-05-22 18:21:44 -0300

    Update settings.py   static

--- educaestagio/educaestagio/settings.py
+++ educaestagio/educaestagio/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 # STATIC_URL = 'https://educastatic-3fbe8.web.app/'
 
-STATIC_URL = 'educa/static/'
+STATIC_URL = '/educaestagio/educa/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/educa/static/'),
+    os.path.join(BASE_DIR, '/educaestagio/educa/static/'),
 ]
 
 
